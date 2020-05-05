9b10295f674a4996a01810f02f5a05fd5e271b6d
SerjiK <serjikcygnati@gmail.com>
2020-05-05 12:23:48 +0300

    p-29 save post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -24,19 +24,19 @@ class PostController extends Controller
             'description'=>'required|min:2|max:1000'
         ]);
 
-        $strpos = strpos($request->photo,';');
-        $sub = substr($request->photo,0,$strpos);
-        $ex = explode('/',$sub)[1];
-        $name = time().".".$ex;
-        $img = Image::make($request->photo)->resize(200, 200);
-        $upload_path = public_path()."/uploadimage/";
-        $img->save($upload_path.$name);
+//        $strpos = strpos($request->photo,';');
+//        $sub = substr($request->photo,0,$strpos);
+//        $ex = explode('/',$sub)[1];
+//        $name = time().".".$ex;
+//        $img = Image::make($request->photo)->resize(200, 200);
+//        $upload_path = public_path()."/uploadimage/";
+//        $img->save($upload_path.$name);
         $post = new Post();
         $post->title = $request->title;
         $post->description = $request->description;
         $post->cat_id = $request->cat_id;
         $post->user_id = Auth::user()->id;
-        $post->photo = $name;
+//        $post->photo = $name;
         $post->save();
     }
 
