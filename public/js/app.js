9e0c70596f1b0ae361f11eb584e8d337a9651169
SerjiK <serjikcygnati@gmail.com>
2020-05-12 00:36:58 +0300

    p-44 delete category using Checkbox

--- public/js/app.js
+++ public/js/app.js
@@ -4439,15 +4439,27 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "List",
-  // data(){
-  //     return{
-  // categoryItem:[],
-  // select:'',
-  // all_select:false
-  //     }
-  // },
+  data: function data() {
+    return {
+      categoryItem: [],
+      select: '',
+      all_select: false
+    };
+  },
   mounted: function mounted() {
     this.$store.dispatch("allCategory");
   },
@@ -4475,30 +4487,37 @@ __webpack_require__.r(__webpack_exports__);
           icon: "success"
         });
       })["catch"](function () {});
-    } // deleteSelected(){
-    //     console.log(this.categoryItem)
-    //     axios.get('/deletecategory/'+this.categoryItem)
-    //         .then(()=>{
-    //             this.categoryItem = []
-    //             this.$store.dispatch("allCategory")
-    //             toast({
-    //                 type: 'success',
-    //                 title: 'Category Deleted successfully'
-    //             })
-    //         })
-    // },
-    // selectAll(){
-    //     if(this.all_select==false){
-    //         this.all_select = true
-    //         for(var category in this.getallCategory){
-    //             this.categoryItem.push(this.getallCategory[category].id)
-    //         }
-    //     }else{
-    //         this.all_select = false
-    //         this.categoryItem = []
-    //     }
-    // }
+    },
+    deleteSelected: function deleteSelected() {
+      var _this2 = this;
 
+      console.log(this.categoryItem);
+      axios.get('/delete-category/' + this.categoryItem).then(function () {
+        _this2.categoryItem = [];
+
+        _this2.$store.dispatch("allCategory"); // toast({
+        //     type: 'success',
+        //     title: 'Category Deleted successfully'
+        // })
+
+
+        _this2.$swal("Category Deleted successfully!", {
+          icon: "success"
+        });
+      });
+    },
+    selectAll: function selectAll() {
+      if (this.all_select == false) {
+        this.all_select = true;
+
+        for (var category in this.getAllCategory) {
+          this.categoryItem.push(this.getAllCategory[category].id);
+        }
+      } else {
+        this.all_select = false;
+        this.categoryItem = [];
+      }
+    }
   }
 });
 
@@ -81109,12 +81128,157 @@ var render = function() {
                   attrs: { id: "example2" }
                 },
                 [
-                  _vm._m(0),
+                  _c("thead", [
+                    _c("tr", [
+                      _c("th", [
+                        _c(
+                          "select",
+                          {
+                            directives: [
+                              {
+                                name: "model",
+                                rawName: "v-model",
+                                value: _vm.select,
+                                expression: "select"
+                              }
+                            ],
+                            attrs: { name: "", id: "" },
+                            on: {
+                              change: [
+                                function($event) {
+                                  var $$selectedVal = Array.prototype.filter
+                                    .call($event.target.options, function(o) {
+                                      return o.selected
+                                    })
+                                    .map(function(o) {
+                                      var val =
+                                        "_value" in o ? o._value : o.value
+                                      return val
+                                    })
+                                  _vm.select = $event.target.multiple
+                                    ? $$selectedVal
+                                    : $$selectedVal[0]
+                                },
+                                _vm.deleteSelected
+                              ]
+                            }
+                          },
+                          [
+                            _c("option", { attrs: { value: "" } }, [
+                              _vm._v("Select")
+                            ]),
+                            _vm._v(" "),
+                            _c("option", { attrs: { value: "" } }, [
+                              _vm._v("Delete all")
+                            ])
+                          ]
+                        ),
+                        _c("br"),
+                        _vm._v(" "),
+                        _c("input", {
+                          directives: [
+                            {
+                              name: "model",
+                              rawName: "v-model",
+                              value: _vm.all_select,
+                              expression: "all_select"
+                            }
+                          ],
+                          attrs: { type: "checkbox" },
+                          domProps: {
+                            checked: Array.isArray(_vm.all_select)
+                              ? _vm._i(_vm.all_select, null) > -1
+                              : _vm.all_select
+                          },
+                          on: {
+                            click: function($event) {
+                              $event.preventDefault()
+                              return _vm.selectAll($event)
+                            },
+                            change: function($event) {
+                              var $$a = _vm.all_select,
+                                $$el = $event.target,
+                                $$c = $$el.checked ? true : false
+                              if (Array.isArray($$a)) {
+                                var $$v = null,
+                                  $$i = _vm._i($$a, $$v)
+                                if ($$el.checked) {
+                                  $$i < 0 &&
+                                    (_vm.all_select = $$a.concat([$$v]))
+                                } else {
+                                  $$i > -1 &&
+                                    (_vm.all_select = $$a
+                                      .slice(0, $$i)
+                                      .concat($$a.slice($$i + 1)))
+                                }
+                              } else {
+                                _vm.all_select = $$c
+                              }
+                            }
+                          }
+                        }),
+                        _vm._v(" "),
+                        _vm.all_select == false
+                          ? _c("span", [_vm._v("Check All")])
+                          : _c("span", [_vm._v("Uncheck All")])
+                      ]),
+                      _vm._v(" "),
+                      _c("th", [_vm._v("S1")]),
+                      _vm._v(" "),
+                      _c("th", [_vm._v("Category name")]),
+                      _vm._v(" "),
+                      _c("th", [_vm._v("Date")]),
+                      _vm._v(" "),
+                      _c("th", [_vm._v("Action")])
+                    ])
+                  ]),
                   _vm._v(" "),
                   _c(
                     "tbody",
                     _vm._l(_vm.getAllCategory, function(category, index) {
                       return _c("tr", { key: category.id }, [
+                        _c("td", [
+                          _c("input", {
+                            directives: [
+                              {
+                                name: "model",
+                                rawName: "v-model",
+                                value: _vm.categoryItem,
+                                expression: "categoryItem"
+                              }
+                            ],
+                            attrs: { type: "checkbox" },
+                            domProps: {
+                              value: category.id,
+                              checked: Array.isArray(_vm.categoryItem)
+                                ? _vm._i(_vm.categoryItem, category.id) > -1
+                                : _vm.categoryItem
+                            },
+                            on: {
+                              change: function($event) {
+                                var $$a = _vm.categoryItem,
+                                  $$el = $event.target,
+                                  $$c = $$el.checked ? true : false
+                                if (Array.isArray($$a)) {
+                                  var $$v = category.id,
+                                    $$i = _vm._i($$a, $$v)
+                                  if ($$el.checked) {
+                                    $$i < 0 &&
+                                      (_vm.categoryItem = $$a.concat([$$v]))
+                                  } else {
+                                    $$i > -1 &&
+                                      (_vm.categoryItem = $$a
+                                        .slice(0, $$i)
+                                        .concat($$a.slice($$i + 1)))
+                                  }
+                                } else {
+                                  _vm.categoryItem = $$c
+                                }
+                              }
+                            }
+                          })
+                        ]),
+                        _vm._v(" "),
                         _c("td", [_vm._v(_vm._s(index + 1))]),
                         _vm._v(" "),
                         _c("td", [_vm._v(_vm._s(category.cat_name))]),
@@ -81134,7 +81298,11 @@ var render = function() {
                                 staticClass: "btn btn-primary",
                                 attrs: { to: "/edit-category/" + category.id }
                               },
-                              [_vm._v("Edit")]
+                              [
+                                _vm._v(
+                                  "\n                                        Edit\n                                    "
+                                )
+                              ]
                             ),
                             _vm._v(" "),
                             _c(
@@ -81167,24 +81335,7 @@ var render = function() {
     ])
   ])
 }
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("thead", [
-      _c("tr", [
-        _c("th", [_vm._v("S1")]),
-        _vm._v(" "),
-        _c("th", [_vm._v("Category name")]),
-        _vm._v(" "),
-        _c("th", [_vm._v("Date")]),
-        _vm._v(" "),
-        _c("th", [_vm._v("Action")])
-      ])
-    ])
-  }
-]
+var staticRenderFns = []
 render._withStripped = true
 
 
--- resources/js/components/admin/category/List.vue
+++ resources/js/components/admin/category/List.vue
@@ -1,75 +1,87 @@
 <template>
     <div>
-    <!-- Main content -->
-    <section class="content">
-        <div class="row justify-content-around">
-            <div class="col-8">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Category list</h3>
-                        <div class="card-tools">
-                            <button class="btn btn-primary">
-                                <router-link to="/add-category" style="color: #FFF">Add category</router-link>
-                            </button>
+        <!-- Main content -->
+        <section class="content">
+            <div class="row justify-content-around">
+                <div class="col-8">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Category list</h3>
+                            <div class="card-tools">
+                                <button class="btn btn-primary">
+                                    <router-link to="/add-category" style="color: #FFF">Add category</router-link>
+                                </button>
+                            </div>
                         </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="example2" class="table table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                    <th>
+                                        <select name="" id="" v-model="select" @change="deleteSelected">
+                                            <option value="">Select</option>
+                                            <option value="">Delete all</option>
+                                        </select><br>
+                                        <input type="checkbox" @click.prevent="selectAll" v-model="all_select">
+                                        <span v-if="all_select==false">Check All</span>
+                                        <span v-else>Uncheck All</span>
+                                    </th>
+                                    <th>S1</th>
+                                    <th>Category name</th>
+                                    <th>Date</th>
+                                    <th>Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr v-for="(category,index) in getAllCategory" :key="category.id">
+                                    <td><input type="checkbox" v-model="categoryItem" :value="category.id"></td>
+                                    <td>{{index+1}}</td>
+                                    <td>{{category.cat_name}}</td>
+                                    <td>{{category.created_at | timeformat}}</td>
+                                    <td>
+                                        <router-link :to="`/edit-category/${category.id}`" class="btn btn-primary">
+                                            Edit
+                                        </router-link>
+                                        <a href="" @click.prevent="deleteCategory(category.id)" class="btn btn-danger">Delete</a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
                     </div>
-                    <!-- /.card-header -->
-                    <div class="card-body">
-                        <table id="example2" class="table table-bordered table-hover">
-                            <thead>
-                            <tr>
-                                <th>S1</th>
-                                <th>Category name</th>
-                                <th>Date</th>
-                                <th>Action</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr v-for="(category,index) in getAllCategory" :key="category.id">
-                                <td>{{index+1}}</td>
-                                <td>{{category.cat_name}}</td>
-                                <td>{{category.created_at | timeformat}}</td>
-                                <td>
-                                    <router-link :to="`/edit-category/${category.id}`" class="btn btn-primary">Edit</router-link>
-                                    <a href="" @click.prevent = "deleteCategory(category.id)" class="btn btn-danger">Delete</a>
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- /.card-body -->
                 </div>
+                <!-- /.col -->
             </div>
-            <!-- /.col -->
-        </div>
-        <!-- /.row -->
-    </section>
-    <!-- /.content -->
+            <!-- /.row -->
+        </section>
+        <!-- /.content -->
     </div>
 </template>
 
 <script>
     export default {
         name: "List",
-        // data(){
-        //     return{
-                // categoryItem:[],
-                // select:'',
-                // all_select:false
-        //     }
-        // },
-        mounted(){
+        data() {
+            return {
+                categoryItem: [],
+                select: '',
+                all_select: false
+            }
+        },
+        mounted() {
             this.$store.dispatch("allCategory")
         },
-        computed:{
-            getAllCategory(){
+        computed: {
+            getAllCategory() {
                 return this.$store.getters.getCategory
             }
         },
-        methods:{
-            deleteCategory(id){
-                axios.get('/category/'+id)
-                    .then(()=>{
+        methods: {
+            deleteCategory(id) {
+                axios.get('/category/' + id)
+                    .then(() => {
                         this.$store.dispatch("allCategory")
                         // toast({
                         //     type: 'success',
@@ -83,32 +95,35 @@
                             icon: "success",
                         });
                     })
-                    .catch(()=>{
+                    .catch(() => {
                     })
             },
-            // deleteSelected(){
-            //     console.log(this.categoryItem)
-            //     axios.get('/deletecategory/'+this.categoryItem)
-            //         .then(()=>{
-            //             this.categoryItem = []
-            //             this.$store.dispatch("allCategory")
-            //             toast({
-            //                 type: 'success',
-            //                 title: 'Category Deleted successfully'
-            //             })
-            //         })
-            // },
-            // selectAll(){
-            //     if(this.all_select==false){
-            //         this.all_select = true
-            //         for(var category in this.getallCategory){
-            //             this.categoryItem.push(this.getallCategory[category].id)
-            //         }
-            //     }else{
-            //         this.all_select = false
-            //         this.categoryItem = []
-            //     }
-            // }
+            deleteSelected() {
+                console.log(this.categoryItem);
+                axios.get('/delete-category/' + this.categoryItem)
+                    .then(() => {
+                        this.categoryItem = [];
+                        this.$store.dispatch("allCategory");
+                        // toast({
+                        //     type: 'success',
+                        //     title: 'Category Deleted successfully'
+                        // })
+                        this.$swal("Category Deleted successfully!", {
+                            icon: "success",
+                        });
+                    })
+            },
+            selectAll() {
+                if (this.all_select == false) {
+                    this.all_select = true
+                    for (var category in this.getAllCategory) {
+                        this.categoryItem.push(this.getAllCategory[category].id)
+                    }
+                } else {
+                    this.all_select = false
+                    this.categoryItem = []
+                }
+            }
         }
     }
 </script>
